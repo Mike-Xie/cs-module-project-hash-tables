e12f4a56fca42dd86c7e84b85e3b7ea2c5e34409
Mike-Xie <Mike-Xie@users.noreply.github.com>
2020-06-03 15:49:55 -0700

    got hist vals by desc and keys by asc

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,26 @@
 # Your code here
 
+ignore_list = ['"', ':', ';', ',', '.','-','+','=','/', '\\', 
+                '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
+ignore_str = ''.join(ignore_list)
+
+with open('robin.txt') as f:
+	# set as a string 
+	my_list = f.read().replace('\n', '')
+
+def word_count(words: str) -> dict:
+    no_punct_words = words.translate(
+        str.maketrans('','',ignore_str)).lower().split()
+    # print(no_punct_words)
+    return dict((word, no_punct_words.count(word)) for word in no_punct_words)
+    # return (reduce( lambda return_dict, 
+    #     count: return_dict.update([(count, return_dict.get(count,0)+1)]) 
+    #     or return_dict, no_punct_words, {}) )
+
+
+x = word_count(my_list)
+# read more here: 
+# https://stackoverflow.com/questions/9919342/sorting-a-dictionary-by-value-then-key
+# 
+y = sorted(x.items(), key = lambda x: (-x[1],x[0]), reverse=True)
+print(y)
--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,18 +1,16 @@
+# imports, they got rid of reduce from standard
 from functools import reduce 
 
+# global vars
+ignore_str = '":;,.-+=/\\|[]{}()*^&'
 
-ignore_list = ['"', ':', ';', ',', '.','-','+','=','/', '\\', 
-                '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
-ignore_str = ''.join(ignore_list)
 def word_count(words: str) -> dict:
     no_punct_words = words.translate(
         str.maketrans('','',ignore_str)).lower().split()
-    print(no_punct_words)
     # return dict((word, no_punct_words.count(word)) for word in no_punct_words)
     return (reduce( lambda return_dict, 
         count: return_dict.update([(count, return_dict.get(count,0)+1)]) 
         or return_dict, no_punct_words, {}) )
-
 
 if __name__ == "__main__":
     print(word_count('Hello, my cat.  And my cat doesn\'t say "hello" back.'))
