8ccffac7fd82607457c8685ee34669d607d27f77
Mike-Xie <Mike-Xie@users.noreply.github.com>
2020-06-03 14:22:22 -0700

    word count passing all tests

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,21 @@
-def word_count(s):
-    # Your code here
+from functools import reduce 
+# import string
 
+ignore_list = ['"', ':', ';', ',', '.','-','+','=','/', '\\', 
+                '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
+ignore_str = ''.join(ignore_list)
+def word_count(words: str):
+    # Your code here
+    # print(words.lower().split())
+    # return dict((word, words.count(word)) for word in words.lower().split())
+    
+    words = words.translate(str.maketrans('','',ignore_str))
+    return (reduce( lambda d, c: d.update([(c, d.get(c,0)+1)]) or d, words.lower().split(), {}) )
 
 
 if __name__ == "__main__":
-    print(word_count(""))
-    print(word_count("Hello"))
+    print(word_count('Hello, my cat.  And my cat doesn\'t say "hello" back.'))
+    print(word_count("Hello hello hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
+    print(word_count("Hello    hello"))
     print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
