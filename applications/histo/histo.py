2b50972d6cd5204c186d8c65c75c2e4a7f49bd54
Mike-Xie <Mike-Xie@users.noreply.github.com>
2020-06-03 17:00:52 -0700

    cleaned up code quality

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,8 +1,5 @@
 # Your code here
-
-ignore_list = ['"', ':', ';', ',', '.','-','+','=','/', '\\', 
-                '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
-ignore_str = ''.join(ignore_list)
+ignore_str = '":;,.-+=/\\|[]{}()*^&'
 
 with open('robin.txt') as f:
 	# set as a string 
@@ -17,10 +14,16 @@ def word_count(words: str) -> dict:
     #     count: return_dict.update([(count, return_dict.get(count,0)+1)]) 
     #     or return_dict, no_punct_words, {}) )
 
+wc_dict = word_count(my_list)
 
-x = word_count(my_list)
+# need longest key length for padding reasons
+longest_key = max(len(x) for x in wc_dict)
+
+# have to sort keys alphabetically which is done with key =, and values by descending which is done with sorted()
 # read more here: 
 # https://stackoverflow.com/questions/9919342/sorting-a-dictionary-by-value-then-key
 # 
-y = sorted(x.items(), key = lambda x: (-x[1],x[0]), reverse=True)
-print(y)
+sorted_tuple = sorted(wc_dict.items(), key = lambda wc_dict: (-wc_dict[1],wc_dict[0]), reverse=False)
+
+for k, v in sorted_tuple:
+	print("{0: <17} ".format(k), v * "#")
